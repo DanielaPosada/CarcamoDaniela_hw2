74c1f7cb7d2d4f69e5fb1a68a13484dddb509cba
DanielaPosada <d.carcamo10@uniandes.edu.co>
2018-07-09 18:40:45 -0500

    CasiFinal

--- CarcamoDaniela_Ciudades.py
+++ CarcamoDaniela_Ciudades.py
@@ -26,62 +26,70 @@ ax4.imshow(triangulos,cmap='gray')
 ax4.autoscale(False)
 plt.savefig('imagenes.pdf')
 #c. Obtener la transformada de Fourier de las cuatro imagenes
-#barce = plt.imread('Barcelona.jpg').astype(float)
-#barce_fft = fft2(barce)
+Barcelona2 = ndimage.imread('Barcelona.jpg', flatten=True)     # flatten=True gives a greyscale image
+FBarcelona = fft2(Barcelona2)
+SBarcelona=fftshift(FBarcelona )
 
-#def plot_spectrum(im_fft):
- #   from matplotlib.colors import LogNorm
-    # A logarithmic colormap
-  #  plt.imshow(np.abs(im_fft), norm=LogNorm(vmin=5))
-   # plt.colorbar()
+Paris2 = ndimage.imread('Paris.jpg', flatten=True) 
+FParis = fft2(Paris2)
+SParis=fftshift(FParis)
 
-#plt.figure()
-#plot_spectrum(barce_fft)
-#plt.title('Fourier transform')
+frac2 = ndimage.imread('frac.jpeg', flatten=True)    
+Ffrac = fft2(frac2)
+Sfrac=fftshift(Ffrac)
 
-#paris= plt.imread('Paris.jpg').astype(float)
-#paris_fft = fft2(paris)
+triangulos2 = ndimage.imread('triangulos.png', flatten=True)  
+Ftriangulos = fft2(triangulos2)
+Striangulos=fftshift(Ftriangulos)
 
-#def plot_spectrum(im_fft):
- #   from matplotlib.colors import LogNorm
-    # A logarithmic colormap
-  #  plt.imshow(np.abs(im_fft), norm=LogNorm(vmin=5))
-   # plt.colorbar()
-
-#plt.figure()
-#plot_spectrum(paris_fft)
-#plt.title('Fourier transform')
-
-#plt.figure()
-image = ndimage.imread('Paris.jpg', flatten=True)     # flatten=True gives a greyscale image
-fft2r = fft2(image)
-shi=fftshift(fft2r)
-plt.figure()
-em=20*np.log(np.abs(shi))
-plt.imshow(em, cmap='gray')
-plt.show()
-
-#f = fft2(Barcelona)
-#
-
-#FBarcelona=fft2(Barcelona)
-#FParis=fft2(Paris)
-#frac=fft2(frac)
-#triangulos=fft2(frac)
 #d. Hacer una grafica con cuatro subplots correspondientes a las transformadas de Fourier de las cuatro imagenes, y guardarla sin mostrarla en transformadaspdf
+fig2 = plt.figure()
+emB=20*np.log(np.abs(SBarcelona))
+emP=20*np.log(np.abs(SParis))
+emfrac=20*np.log(np.abs(Sfrac))
+emtri=20*np.log(np.abs(Striangulos))
+ay = fig2.add_subplot(2, 2, 1)
+ay.imshow(emB, cmap='gray')
+ay.autoscale(False)
+ay2 = fig2.add_subplot(2, 2, 2)
+ay2.imshow(emP,cmap='gray')
+ay2.autoscale(False)
+ay3 = fig2.add_subplot(2, 2, 3)
+ay3.imshow(emfrac,cmap='gray')
+ay3.autoscale(False)
+ay4 = fig2.add_subplot(2, 2, 4)
+ay4.imshow(emtri,cmap='gray')
+ay4.autoscale(False)
+plt.savefig('transformadas.pdf')
+
+#e. Hacer otra grafica con cuatro subplots que correspondan a un corte transversal horizontal en el centro de las graficas de las transformadas de Fourier anteriores, y guardarla sinmostrarla en cortes_transversalespdf
+fig3 = plt.figure()
+copiaB=abs(FBarcelona.astype(dtype=np.float))
+mitadB=int(copiaB.shape[0]/2)
+ax = fig3.add_subplot(2, 2, 1)
+ax.plot(copiaB[mitadB,:])
+ax.autoscale(True)
+
+copiaP=abs(FParis.astype(dtype=np.float))
+mitadP=int(copiaP.shape[0]/2)
+ax2 = fig3.add_subplot(2, 2, 2)
+ax2.plot(copiaP[mitadP,:])
+ax2.autoscale(True)
+
+copiaf=abs(Ffrac.astype(dtype=np.float))
+mitadf=int(copiaf.shape[0]/2)
+ax3 = fig3.add_subplot(2, 2, 3)
+ax3.plot(copiaf[mitadf,:])
+ax3.autoscale(True)
+
+copiat=abs(Ftriangulos.astype(dtype=np.float))
+mitadt=int(copiat.shape[0]/2)
+ax4 = fig3.add_subplot(2, 2, 4)
+ax4.plot(copiat[mitadt,:])
+ax4.autoscale(True)
+
+plt.savefig('cortes_transversales.pdf')
 
 
-#ax2.plot(Paris, cmap='gray')
-#ax3.plot(frac, cmap='gray')
-#ax4.plot(tri, cmap='gray')
-#
+#h. Para la imagen Barcelona.jpg, construya un filtro que le permita borrar las vias horizontales del mapa, dejando las verticales. Guarde la imagen obtenida sin vias horizontalesen sin_horizontales.pdf.
 
-
-
-#I8 = (((I - I.min()) / (I.max() - I.min())) * 255.9).astype(np.uint8)
-
-#img = Image.fromarray(I8)
-#img.save("file.png")
-
-
-#print(np.shape(img))
