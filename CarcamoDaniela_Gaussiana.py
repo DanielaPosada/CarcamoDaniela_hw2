6f0995dd74824ca082092b7a6e84f0f9f423dc8b
DanielaPosada <d.carcamo10@uniandes.edu.co>
2018-07-09 19:47:53 -0500

    Last

--- CarcamoDaniela_Gaussiana.py
+++ CarcamoDaniela_Gaussiana.py
@@ -1,19 +1,22 @@
 import numpy as np
 N=3
 Arreglo=(np.random.random((N,N))*10.0)-5.0
+print "Matriz A"
 print Arreglo
 B=(np.random.random((N,1))*10.0)-5.0
+print "Vector B"
 print B
 
 #Escriba un codigo de eliminacion Gaussiana para resolver el sistema Ax=B.
 
 sol = np.linalg.solve(Arreglo,B)
-print np.dot(Arreglo,sol)
-print("sol",sol)
+
+print("Vector solucion sin algoritmo",sol)
 
 Arreglo1=np.ones((N,N+1),dtype=np.float)
 Arreglo1[:,0:N]=Arreglo
 Arreglo1[:,N]=B[:,0]
+print "Copia de la matriz con vector solucion incluido"
 print Arreglo1
 
 numero=1
@@ -27,6 +30,7 @@ for k in range(N-1):
 				Arreglo1[k,:]=Arreglo1[k+1,:]
 				Arreglo1[k+1,:]=guarda
 	numero=numero+1
+print "Matriz Triangular Superior"
 print Arreglo1
 
 numero2=N-1
@@ -36,7 +40,10 @@ for i in xrange(N-1,-1,-1):
 	numero2=numero2-1
 	for k in xrange(numero2,-1,-1): 
 		Arreglo1[k,:]=Arreglo1[k,:]-Arreglo1[k,i]*Arreglo1[i,:]		
+print "La matriz solucionada es:"
 print Arreglo1
+
+print "El vector solucion del sistema con mi implementacion es:", Arreglo1[:,N]
 
 
 		
